517a62034b4ba48d52e39fe02bf6cb810b71839a
Luke Li <3813052+lukeligae@users.noreply.github.com>
2024-08-13 15:49:01 -0700

    Create SQL_Scripts_with_Jinja_Variable_In_Repo_sql_Set.sql

---
+++ Working with Git/SQL_Scripts_with_Jinja_Variable_In_Repo_sql_Set.sql
@@ -0,0 +1,19 @@
+/*
+This scripts file can't run in Worksheet.
+The line "{% set env = 'dev' %}" will throww  Syntax error: unexpected '%'.
+
+1. If use the set the Jinja variable with Jinja syntax, {% set env='dev' %}
+   It can't run in worksheet, and it will throw  Syntax error: unexpected '%'.
+
+2.  If use the set the Jinja variable with SQL syntax, SET env = 'dev';
+    It can run in worksheeet without error  but the Jinja variable vaule will not be replaced.
+    
+
+*/
+
+--{% set env='dev' %}
+SET env = 'dev';
+
+SELECT 'CREATE DATABASE  DB_{{env}};' as COL
+UNION ALL
+SELECT 'CREATE SCHEMA  SCHEMA_{{env}};' AS COL;
